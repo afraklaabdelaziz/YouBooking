1237c6264cd7420eeb21b9feb1a6a9af55b27c05
afraklaabdelaziz <afraklaabdelaziz@gmail.com>
2022-12-30 11:29:54 +0100

    ajouter les methods de crud dans controller hotel

--- src/main/java/com/example/youbooking/controllers/HotelController.java
+++ src/main/java/com/example/youbooking/controllers/HotelController.java
@@ -1,9 +1,47 @@
 package com.example.youbooking.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.youbooking.dto.HotelDto;
+import com.example.youbooking.entities.Hotel;
+import com.example.youbooking.services.IHotelService;
+import com.example.youbooking.services.dto.ResponseDTO;
+import com.example.youbooking.utiles.DtoToEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/hotel")
 public class HotelController {
+
+    @Autowired
+    IHotelService hotelService;
+
+    @GetMapping
+    public ResponseDTO getAll(){
+        return hotelService.findAllHotels();
+    }
+
+    @GetMapping("/one/{id}")
+    public ResponseDTO findOne(@PathVariable Long id){
+        return hotelService.findOneHotel(id);
+    }
+
+    @PostMapping("/add")
+    public ResponseDTO addChamber(@Valid HotelDto hotelDto){
+        Hotel hotel = DtoToEntity.hoteDtoToHotel(hotelDto);
+        return hotelService.addHotel(hotel);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseDTO updateChamber(@Valid HotelDto hotelDto,@PathVariable Long id){
+        hotelDto.setId(id);
+        Hotel hotel = DtoToEntity.hoteDtoToHotel(hotelDto);
+        return hotelService.updateHotel(hotel);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseDTO deleteChamber(@PathVariable Long id){
+        return hotelService.deleteHotel(id);
+    }
 }
