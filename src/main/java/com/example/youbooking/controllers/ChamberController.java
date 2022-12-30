ea2329cf6400692c5dcae29bfb67da51bfcba69e
afraklaabdelaziz <afraklaabdelaziz@gmail.com>
2022-12-30 11:25:02 +0100

    ajouter les methode add,delete,update,findAll dans controller chamber

--- src/main/java/com/example/youbooking/controllers/ChamberController.java
+++ src/main/java/com/example/youbooking/controllers/ChamberController.java
@@ -1,10 +1,48 @@
 package com.example.youbooking.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.youbooking.dto.ChamberDto;
+import com.example.youbooking.entities.Chamber;
+import com.example.youbooking.services.IChamberService;
+import com.example.youbooking.services.dto.ResponseDTO;
+import com.example.youbooking.utiles.DtoToEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/chamber")
 public class ChamberController {
+
+    @Autowired
+    IChamberService chamberService;
+
+    @GetMapping
+    public ResponseDTO getAll(){
+        return chamberService.findAllChamber();
+    }
+
+    @GetMapping("/one/{id}")
+    public ResponseDTO findOne(@PathVariable Long id){
+        return chamberService.findOneChamber(id);
+    }
+
+    @PostMapping("/add")
+    public ResponseDTO addChamber(@Valid ChamberDto chamberDto){
+        Chamber chamber = DtoToEntity.chamberDtoToChamber(chamberDto);
+        return chamberService.addChamber(chamber);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseDTO updateChamber(@Valid ChamberDto chamberDto,@PathVariable Long id){
+        chamberDto.setId(id);
+        Chamber chamber = DtoToEntity.chamberDtoToChamber(chamberDto);
+        return chamberService.updateChamber(chamber);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseDTO deleteChamber(@PathVariable Long id){
+        return chamberService.deleteChamber(id);
+    }
 
 }
--- src/main/java/com/example/youbooking/utiles/DtoToEntity.java
+++ src/main/java/com/example/youbooking/utiles/DtoToEntity.java
@@ -1,7 +1,10 @@
 package com.example.youbooking.utiles;
 
+import com.example.youbooking.dto.AdresseDto;
+import com.example.youbooking.dto.ChamberDto;
+import com.example.youbooking.dto.HotelDto;
 import com.example.youbooking.dto.UserDto;
-import com.example.youbooking.entities.User;
+import com.example.youbooking.entities.*;
 import org.modelmapper.ModelMapper;
 
 public class DtoToEntity {
@@ -9,4 +12,20 @@ public class DtoToEntity {
         ModelMapper modelMapper=new ModelMapper();
         return modelMapper.map(userDto, User.class);
     }
+
+    public static Chamber chamberDtoToChamber(ChamberDto chamberDto) {
+        ModelMapper modelMapper=new ModelMapper();
+        return modelMapper.map(chamberDto, Chamber.class);
+    }
+
+    public static Hotel hoteDtoToHotel(HotelDto hotelDto) {
+        ModelMapper modelMapper=new ModelMapper();
+        return modelMapper.map(hotelDto, Hotel.class);
+    }
+
+    public static Adresse adresseDtoToAdresse(AdresseDto adresseDto) {
+        ModelMapper modelMapper=new ModelMapper();
+        return modelMapper.map(adresseDto, Adresse.class);
+    }
+
 }
